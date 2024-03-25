4a0d9d1633110a9631f814f2b367ad7ca80cc701
Yashvi Sharma <yashuvatsas@gmail.com>
2024-03-25 09:40:06 -0700

    testing wavesol

--- pysedm/sedmkpvariables.py
+++ pysedm/sedmkpvariables.py
@@ -13,7 +13,7 @@ SEDM_ROT    = 300 # SEDM alignment to have north up
 SEDM_MLA_RADIUS = 25
 
 # --- LBDA
-SEDM_LBDA = np.linspace(3700, 9300, 220)
+SEDM_LBDA = np.linspace(4600, 9300, 220)
 LBDA_PIXEL_CUT = 3
 
 DEFAULT_REFLBDA = 6000 # In Angstrom
--- pysedm/wavesolution.py
+++ pysedm/wavesolution.py
@@ -1170,7 +1170,7 @@ class VirtualArcSpectrum( BaseObject ):
             self._normguesses["ampl%d_boundaries"%i] = [self.arclines[l]["ampl"]*0.7, self.arclines[l]["ampl"]*3]
             
             self._normguesses["mu%d_guess"%i]        = self.arclines[l]["mu"]+lines_shift
-            self._normguesses["mu%d_boundaries"%i]   = [self._normguesses["mu%d_guess"%i]-5, self._normguesses["mu%d_guess"%i]+5]
+            self._normguesses["mu%d_boundaries"%i]   = [self._normguesses["mu%d_guess"%i]-1, self._normguesses["mu%d_guess"%i]+4]
             
             self._normguesses["sig%d_guess"%i]       = 1.1 if (not "doublet" in self.arclines[l] or not self.arclines[l]["doublet"]) else 1.8
             self._normguesses["sig%d_boundaries"%i]  = [0.9,1.5] if (not "doublet" in self.arclines[l] or not self.arclines[l]["doublet"]) else [1.1, 3]
